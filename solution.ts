3cafadd5acb7f7f2bbbeab1e473fadce2db3b037
Jannatul Fardous Nila <jannatulnila74@gmail.com>
2025-11-18 18:31:40 +0600

    Add utility functions and Person class

--- solution.ts
+++ solution.ts
@@ -1,1 +1,151 @@
-console.log("hello")
+function formatValue(value: string | number | boolean): string | number | boolean {
+  if (typeof value === "string") {
+    return value.toUpperCase();
+  } else if (typeof value === "number") {
+    return value * 10;
+  } else {
+    return !value;
+  }
+}
+
+
+function getLength(value: string | any[]): number {
+  if (typeof value === "string") {
+    return value.length;
+  } else if (Array.isArray(value)) {
+    return value.length;
+  }
+  return 0;
+}
+
+
+class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return `'Name: ${this.name}, Age: ${this.age}'`;
+  }
+}
+
+const person1 = new Person("John Doe", 30);
+
+
+
+type Item = {
+  title: string;
+  rating: number;
+};
+
+function filterByRating(items: Item[]): Item[] {
+  return items.filter(item => item.rating >= 4);
+}
+
+const books = [
+  { title: "Book A", rating: 4.5 },
+  { title: "Book B", rating: 3.2 },
+  { title: "Book C", rating: 5.0 },
+];
+
+
+
+type User = {
+  id: number;
+  name: string;
+  email: string;
+  isActive: boolean;
+};
+
+function filterActiveUsers(users: User[]): User[] {
+  return users.filter(user => user.isActive === true);
+}
+
+const users = [
+  { id: 1, name: "Rakib", email: "rakib@example.com", isActive: true },
+  { id: 2, name: "Asha", email: "asha@example.com", isActive: false },
+  { id: 3, name: "Rumi", email: "rumi@example.com", isActive: true }
+];
+
+const person2 = new Person("Alice", 25);
+
+
+
+interface Book {
+  title: string;
+  author: string;
+  publishedYear: number;
+  isAvailable: boolean;
+}
+
+function printBookDetails(book: Book): string {
+  const available = book.isAvailable ? "Yes" : "No";
+  return `Title: ${book.title}, Author: ${book.author}, Published: ${book.publishedYear}, Available: ${available}`;
+}
+
+const myBook: Book = {
+  title: "The Great Gatsby",
+  author: "F. Scott Fitzgerald",
+  publishedYear: 1925,
+  isAvailable: true,
+};
+
+printBookDetails(myBook); 
+
+
+
+ function getUniqueValues(arr1: (string | number)[], arr2: (string | number)[]): (string | number)[] {
+  const result: (string | number)[] = [];
+
+  function pushUnique(value: string | number) {
+    // manually check duplicate
+    let exists = false;
+    for (let i = 0; i < result.length; i++) {
+      if (result[i] === value) {
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) result.push(value);
+  }
+
+  for (let v of arr1) pushUnique(v);
+  for (let v of arr2) pushUnique(v);
+
+  return result;
+}
+
+
+
+type Product = {
+  name: string;
+  price: number;
+  quantity: number;
+  discount?: number;
+};
+
+function calculateTotalPrice(products: Product[]): number {
+  if (products.length === 0) return 0;
+
+  return products
+    .map(product => {
+      let total = product.price * product.quantity;
+
+      if (product.discount) {
+        total = total - (total * product.discount) / 100;
+      }
+
+      return total;
+    })
+    .reduce((sum, value) => sum + value, 0);
+}
+
+const products = [
+  { name: "Pen", price: 10, quantity: 2 },
+  { name: "Notebook", price: 25, quantity: 3, discount: 10 },
+  { name: "Bag", price: 50, quantity: 1, discount: 20 },
+];
